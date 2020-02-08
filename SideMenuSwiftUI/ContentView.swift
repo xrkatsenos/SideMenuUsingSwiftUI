577d1cc1e2f086cab0b86088ba0bb8fd78e53287
Christos Katsenos <christos.katsenos@ericsson.com>
2020-02-08 16:20:11 +0200

    Adding icons settings

--- SideMenuSwiftUI/ContentView.swift
+++ SideMenuSwiftUI/ContentView.swift
@@ -11,7 +11,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+        
+        
+        
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
